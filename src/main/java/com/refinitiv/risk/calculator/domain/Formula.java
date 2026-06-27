1a9a59f19b2763a145dfcfe2ceba2041dd768761
Tisana Roongsawang <tisana@gmail.com>
2026-06-27 23:01:01 +0700

    Replace manual getters and setters with Lombok annotations in Formula and Result classes

--- src/main/java/com/refinitiv/risk/calculator/domain/Formula.java
+++ src/main/java/com/refinitiv/risk/calculator/domain/Formula.java
@@ -1,22 +1,12 @@
 package com.refinitiv.risk.calculator.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Formula {
     Double x;
     Double y;
 
-    public Double getX() {
-        return x;
-    }
-
-    public void setX(Double x) {
-        this.x = x;
-    }
-
-    public Double getY() {
-        return y;
-    }
-
-    public void setY(Double y) {
-        this.y = y;
-    }
 }
--- src/main/java/com/refinitiv/risk/calculator/domain/Result.java
+++ src/main/java/com/refinitiv/risk/calculator/domain/Result.java
@@ -1,22 +1,12 @@
 package com.refinitiv.risk.calculator.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Result {
     private String operation;
     private Double result;
 
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public Double getResult() {
-        return result;
-    }
-
-    public void setResult(Double result) {
-        this.result = result;
-    }
 }
